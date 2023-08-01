0e6f18f9cf49eee38f42cd235641d92e09228bb4
John Lockwood <john@codesolid.com>
2023-08-01 10:57:16 -0400

    Steps 6 and 7 of CMake Tutorial

--- cmake_tutorial_solutions/source/Step7/MathFunctions/mysqrt.cxx
+++ cmake_tutorial_solutions/source/Step7/MathFunctions/mysqrt.cxx
@@ -1,6 +1,7 @@
 #include "mysqrt.h"
 
 #include <iostream>
+#include <cmath>
 
 namespace mathfunctions {
 namespace detail {
@@ -11,12 +12,11 @@ double mysqrt(double x)
     return 0;
   }
 
-  // TODO 5: If both HAVE_LOG and HAVE_EXP are defined,  use the following:
-  //// double result = std::exp(std::log(x) * 0.5);
-  //// std::cout << "Computing sqrt of " << x << " to be " << result
-  ////        << " using log and exp" << std::endl;
-  // else, use the existing logic.
-
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = std::exp(std::log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+    << " using log and exp" << std::endl;
+#else
   // Hint: Don't forget the #endif before returning the result!
 
   double result = x;
@@ -30,7 +30,7 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-
+#endif
   return result;
 }
 }
